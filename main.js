e3a979597166c5530ee601d9562144dbad279629
edman <compFoxLord007@gmail.com>
2022-04-25 14:53:55 +0400

    adding number data type chacking

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ function el(type, attrs, children) {
 
     type = type.toUpperCase();
 
-    if (typeof children === 'string') {
+    if (typeof children === 'string' || typeof children === 'number') {
         
-        // changing string data to an object instance   
+        // changing string or number data to an object instance   
         children = new CLASSES['TEXT']('TEXT', children);
     }
 
@@ -15,3 +15,7 @@ function el(type, attrs, children) {
     return new CLASSES[type](type, attrs, children);
 
 }
+
+const tree = el('span', {class: 'span'}, 101);
+
+document.getElementById('root').appendChild(tree.draw());
