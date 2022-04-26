c2b1fe87244f38d693ab095cb42ac15b314f676d
edman <compFoxLord007@gmail.com>
2022-04-27 00:20:07 +0400

    Added checking the type of the created element and checking possible attributes of element

--- src/DomElement.js
+++ src/DomElement.js
@@ -5,6 +5,34 @@ export default class DomElement {
         this.type = type;
         this.props = props || {};
         this.children = children || [];
+
+        this._primalType = 'DOMELEMENT';
+        this._possibleAttributes = [
+            'id', 'class', 'style', 'hidden', 'title' 
+        ]
+    }
+
+    _checkType() {
+        // Checks for correctness of the class type.
+        // For example: Can't create DivElement 
+        // if passed type to the constructor is 'INPUT'.
+        if (this.type.toUpperCase() !== this._primalType) {
+            throw new Error('The element type does not match the class type!');
+        }
+    }
+
+    _checkPossibleAttributes() {
+        // Checks the attributes that a particular element can have.
+        for (let attr of Object.keys(this.props)) {
+            if (!this._possibleAttributes.includes(attr.toLowerCase())) {
+                throw new Error(`Can't add attribute "${attr}" to ${this._primalType}`);
+            }
+        }
+    }
+
+    _addAttributes(...attr) {
+        // Adds attributes that match the desired class.
+        this._possibleAttributes = [...this._possibleAttributes, ...attr];
     }
 
     draw() {
--- src/representationClasses.js
+++ src/representationClasses.js
@@ -1,33 +1,136 @@
 import DomElement from "./DomElement.js";
 
 
-class DivElement extends DomElement {}
+class DivElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'DIV';
+        this._checkType();
+        this._checkPossibleAttributes();
+    }
+}
 
-class SpanElement extends DomElement {}
+class SpanElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'SPAN';
+        this._checkType();
+        this._checkPossibleAttributes();
+    }
+}
 
-class UlElement extends DomElement {}
+class UlElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'UL';
+        this._checkType();
+        this._addAttributes('type');
+        this._checkPossibleAttributes();
+    }
+}
 
-class OlElement extends DomElement {}
+class OlElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'OL';
+        this._checkType();
+        this._addAttributes('type', 'reversed', 'start');
+        this._checkPossibleAttributes();
+    }
+}
 
-class LiElement extends DomElement {}
+class LiElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'LI';
+        this._checkType();
+        this._addAttributes('type', 'value');
+        this._checkPossibleAttributes();
+    }
+}
 
-class PElement extends DomElement {}
+class PElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'P';
+        this._checkType();
+        this._addAttributes('align');
+        this._checkPossibleAttributes();
+    }
+}
 
-class AElement extends DomElement {}
+class AElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'A';
+        this._checkType();
+        this._addAttributes('href', 'target', 'type');
+        this._checkPossibleAttributes();
+    }
+}
 
-class FormElement extends DomElement {}
+class FormElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'FORM';
+        this._checkType();
+        this._addAttributes(
+            'action', 'autocomplete', 'method', 'name', 'enctype'
+        );
+        this._checkPossibleAttributes();
+    }
+}
 
-class LabelElement extends DomElement {}
+class LabelElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'LABEL';
+        this._checkType();
+        this._addAttributes('for');
+        this._checkPossibleAttributes();
+    }
+}
 
-class BrElement extends DomElement {}
+class BrElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'BR';
+        this._checkType();
+        this._addAttributes('clear');
+        this._checkPossibleAttributes();
+    }
+}
 
-class ButtonElement extends DomElement {}
+class ButtonElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'BUTTON';
+        this._checkType();
+        this._addAttributes(
+            'name', 'type', 'value', 'disabled', 'form', 'formaction'
+        );
+        this._checkPossibleAttributes();
+    }
+}
 
-class InputElement extends DomElement {}
+class InputElement extends DomElement {
+    constructor(type, props, children) {
+        super(type, props, children);
+        this._primalType = 'INPUT';
+        this._checkType();
+        this._addAttributes(
+            'type', 'value', 'form', 'formaction',
+            'disabled', 'name', 'placeholder', 'required'
+        );
+        this._checkPossibleAttributes();
+    }
+}
 
 class TextElement extends DomElement {
     constructor(type, value) {
         super(type, {}, value);
+        this._primalType = 'TEXT';
+        this._checkType();
     }
 }
 
